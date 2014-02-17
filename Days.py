763561461de6957e651423750e46e2058c03b40f
wryman <william.ryman@utexas.edu>
2014-02-16 23:39:51 -0600

    Start

---
+++ Days.py
@@ -0,0 +1,26 @@
+#  File: Day.py
+
+#  Description:
+
+#  Student Name:William Ryman
+
+#  Student UT EID:wrr368
+
+#  Course Name: CS 303E
+
+#  Unique Number:53465 
+
+#  Date Created:2/16/14
+
+#  Date Last Modified:
+
+def main():
+  
+  a=int(input('Enter month:'))
+  w = (13 * a - 1 ) // 5
+  x = c // 4 
+  y = d // 4
+  z = w + x + y + b + c - 2 * d
+  r = z % 7
+  r = (r + 7) % 7
+main()
